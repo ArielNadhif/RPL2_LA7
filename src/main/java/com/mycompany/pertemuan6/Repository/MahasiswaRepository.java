b6ef746ebbed830fc73b2003379dc9388a4243d1
Ariel <arielnadhif799@gmail.com>
2025-11-30 16:03:11 +0700

    first commit

---
+++ src/main/java/com/mycompany/pertemuan6/Repository/MahasiswaRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pertemuan6.Repository;
+
+import com.mycompany.pertemuan6.Model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author ASUS
+ */
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Integer> {
+    
+
+}
+
